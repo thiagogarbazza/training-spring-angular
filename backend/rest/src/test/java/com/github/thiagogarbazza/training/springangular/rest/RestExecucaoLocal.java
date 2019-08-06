ccf396d707359432a04d5652318a980b7aa918e2
Thiago Garbazza <thiagogarbazza@gmail.com>
2019-08-06 18:08:56 -0300

    Configurando embeded jetty

--- backend/rest/src/test/java/com/github/thiagogarbazza/training/springangular/rest/RestExecucaoLocal.java
+++ backend/rest/src/test/java/com/github/thiagogarbazza/training/springangular/rest/RestExecucaoLocal.java
@@ -1,15 +1,47 @@
 package com.github.thiagogarbazza.training.springangular.rest;
 
-import com.github.thiagogarbazza.training.springangular.rest.it.RestIntegrationTestRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
-@WebAppConfiguration
-@RestIntegrationTestRunner
-public class RestExecucaoLocal {
+class RestExecucaoLocal {
 
-  public static void main(String[] args) {
-    //  final ConfigurableApplicationContext context = new SpringApplicationBuilder()
-    //  .sources(RestExecucaoLocal.class)
-    //.run(args);
+  private final int PORT = 8080;
+
+  private RestExecucaoLocal() {
+
+  }
+
+  ServletContextHandler servletContextBuilder(final WebApplicationContext webAppContext) {
+    ServletContextHandler contextHandler = new ServletContextHandler();
+
+    DispatcherServlet dispatcherServlet = new DispatcherServlet(webAppContext);
+    ServletHolder springServletHolder = new ServletHolder(dispatcherServlet);
+    contextHandler.addServlet(springServletHolder, "/*");
+    contextHandler.addEventListener(new ContextLoaderListener(webAppContext));
+
+    return contextHandler;
+  }
+
+  void start(WebApplicationContext webAppContext) throws Exception {
+    Server server = new Server(PORT);
+
+    server.setHandler(servletContextBuilder(webAppContext));
+
+    server.start();
+  }
+
+  public static void main(String[] args) throws Exception {
+    new RestExecucaoLocal().start(contextBuilder());
+  }
+
+  private static WebApplicationContext contextBuilder() {
+    AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+    context.register(RestExecucaoLocalConfiguration.class);
+    return context;
   }
 }
---
+++ backend/rest/src/test/java/com/github/thiagogarbazza/training/springangular/rest/RestExecucaoLocalConfiguration.java
@@ -0,0 +1,12 @@
+package com.github.thiagogarbazza.training.springangular.rest;
+
+import com.github.thiagogarbazza.training.springangular.utiltest.UtilTestConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@WebAppConfiguration
+@Import({RestConfiguration.class, UtilTestConfiguration.class})
+@PropertySource("classpath:application-rest-integration-test.properties")
+class RestExecucaoLocalConfiguration {
+}
