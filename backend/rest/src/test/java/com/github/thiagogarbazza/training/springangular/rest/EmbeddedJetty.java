01af546ebebd64f7075e138d6cc247791c1a89d5
Thiago Garbazza <thiagogarbazza@gmail.com>
2019-08-08 09:23:06 -0300

    Refactory jetty embedded

---
+++ backend/rest/src/test/java/com/github/thiagogarbazza/training/springangular/rest/EmbeddedJetty.java
@@ -0,0 +1,58 @@
+package com.github.thiagogarbazza.training.springangular.rest;
+
+import lombok.extern.apachecommons.CommonsLog;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@CommonsLog
+class EmbeddedJetty {
+
+  private final Server server;
+
+  public EmbeddedJetty(final int port) {
+    this.server = new Server(port);
+  }
+
+  public void run() {
+    server.setHandler(servletContextBuilder());
+
+    try {
+      server.start();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public void stop() {
+    try {
+      server.stop();
+      server.join();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private ContextHandler servletContextBuilder() {
+    ServletContextHandler contextHandler = new ServletContextHandler();
+
+    final WebApplicationContext webApplicationContext = webApplicationContextBuilder();
+    DispatcherServlet dispatcherServlet = new DispatcherServlet(webApplicationContext);
+    ServletHolder springServletHolder = new ServletHolder(dispatcherServlet);
+    contextHandler.addServlet(springServletHolder, "/*");
+    contextHandler.addEventListener(new ContextLoaderListener(webApplicationContext));
+
+    return contextHandler;
+  }
+
+  private static WebApplicationContext webApplicationContextBuilder() {
+    AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+    context.register(RestExecucaoLocalConfiguration.class);
+    return context;
+  }
+}
--- backend/rest/src/test/java/com/github/thiagogarbazza/training/springangular/rest/RestExecucaoLocal.java
+++ backend/rest/src/test/java/com/github/thiagogarbazza/training/springangular/rest/RestExecucaoLocal.java
@@ -1,47 +1,14 @@
 package com.github.thiagogarbazza.training.springangular.rest;
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
+import lombok.extern.apachecommons.CommonsLog;
 
+@CommonsLog
 class RestExecucaoLocal {
 
-  private final int PORT = 8080;
+  private static final int PORT = 9090;
+  private static final String contextPath = "";
 
-  private RestExecucaoLocal() {
-
-  }
-
-  ServletContextHandler servletContextBuilder(final WebApplicationContext webAppContext) {
-    ServletContextHandler contextHandler = new ServletContextHandler();
-
-    DispatcherServlet dispatcherServlet = new DispatcherServlet(webAppContext);
-    ServletHolder springServletHolder = new ServletHolder(dispatcherServlet);
-    contextHandler.addServlet(springServletHolder, "/*");
-    contextHandler.addEventListener(new ContextLoaderListener(webAppContext));
-
-    return contextHandler;
-  }
-
-  void start(WebApplicationContext webAppContext) throws Exception {
-    Server server = new Server(PORT);
-
-    server.setHandler(servletContextBuilder(webAppContext));
-
-    server.start();
-  }
-
-  public static void main(String[] args) throws Exception {
-    new RestExecucaoLocal().start(contextBuilder());
-  }
-
-  private static WebApplicationContext contextBuilder() {
-    AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-    context.register(RestExecucaoLocalConfiguration.class);
-    return context;
+  public static void main(String[] args) {
+    new EmbeddedJetty(PORT).run();
   }
 }
